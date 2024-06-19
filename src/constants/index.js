97bbdbc6513fe5ece6520f7494d794e57778cd26
Adesodun Titilope Temiloluwa <115459326+adesodun27@users.noreply.github.com>
2024-06-19 12:57:59 +0100

    fix changes commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -36,7 +36,7 @@ export const frame1 = [
     image: Frame,
     Header: "Homyz",
     details:
-      "I contributed to Westway World's frontend development, creating a user-friendly platform for global youth. It enables users to discover WestWay company's upcoming events and conveniently buy event-related merchandise. Leveraging ReactJS, I integrated dynamic functionalities, while MaterialUI ensured seamless and responsive designs.",
+      "Developed a full-stack real estate web application to manage property listings, user authentication, and advanced search functionalities. This project significantly enhanced my skills in using React, including its setup and integration within a full-stack environment.",
     liveProjectUrl: "https://real-estate-ten-gamma.vercel.app/",
     githubRepoUrl: "https://github.com/adesodun27/Real-Estate",
   },
@@ -44,7 +44,7 @@ export const frame1 = [
     image: Frame1,
     Header: "Todo",
     details:
-      "I contributed to Westway World's frontend development, creating a user-friendly platform for global youth. It enables users to discover WestWay company's upcoming events and conveniently buy event-related merchandise. Leveraging ReactJS, I integrated dynamic functionalities, while MaterialUI ensured seamless and responsive designs.",
+      " I created a to-do list application by leveraging tutorials and guidance from YouTube videos. This project improved my ability to learn and implement new technologies independently. It also helped me refine my skills in JavaScript, CSS, and HTML, and understand best practices for front-end development, including user experience design and responsive layout techniques.",
     liveProjectUrl: "https://todoapp-wheat-omega.vercel.app/",
     githubRepoUrl: "https://github.com/adesodun27/todoapp",
   },
@@ -52,7 +52,7 @@ export const frame1 = [
     image: Frame2,
     Header: "Acme",
     details:
-      "I contributed to Westway World's frontend development, creating a user-friendly platform for global youth. It enables users to discover WestWay company's upcoming events and conveniently buy event-related merchandise. Leveraging ReactJS, I integrated dynamic functionalities, while MaterialUI ensured seamless and responsive designs.",
+      "Reverse-engineered and developed a website from scratch to deepen my understanding of web development fundamentals. This project involved analyzing an existing website and replicating its functionality and design. Through this process, I gained valuable insights into structuring HTML, styling with CSS, and creating responsive layouts.",
     liveProjectUrl: "https://acme-web-design-two.vercel.app",
     githubRepoUrl: "https://github.com/adesodun27/acme-web-design",
   },
@@ -60,7 +60,7 @@ export const frame1 = [
     image: Frame3,
     Header: "Luxury Jewelry",
     details:
-      "I contributed to Westway World's frontend development, creating a user-friendly platform for global youth. It enables users to discover WestWay company's upcoming events and conveniently buy event-related merchandise. Leveraging ReactJS, I integrated dynamic functionalities, while MaterialUI ensured seamless and responsive designs.",
+      "Designed and implemented a user-friendly landing page for a Luxury Jewelry brand as part of an assignment from Coursera. This project allowed me to showcase my skills in front-end development, focusing on creating an engaging and visually appealing interface using HTML and CSS. By incorporating best practices in web design, such as responsive layout techniques and aesthetic considerations, I aimed to deliver a seamless user experience.",
     liveProjectUrl: "https://cousera-project-eta.vercel.app/",
     githubRepoUrl: "https://github.com/adesodun27/cousera-project",
   },
@@ -68,7 +68,7 @@ export const frame1 = [
     image: Frame4,
     Header: "Nike",
     details:
-      "I contributed to Westway World's frontend development, creating a user-friendly platform for global youth. It enables users to discover WestWay company's upcoming events and conveniently buy event-related merchandise. Leveraging ReactJS, I integrated dynamic functionalities, while MaterialUI ensured seamless and responsive designs.",
+      "Independently developed a responsive Nike product page, leveraging tutorials and guidance from YouTube videos. This project marked my introduction to using Tailwind CSS and further solidified my proficiency in front-end development. By implementing HTML, CSS, and integrating React for dynamic components, I focused on creating a visually compelling and user-friendly interface. This experience enhanced my understanding of responsive design principles, component-based architecture, and utilizing Tailwind CSS for efficient styling.",
     liveProjectUrl: "https://tailwind-nine-sigma.vercel.app/",
     githubRepoUrl: "https://github.com/adesodun27/tailwind",
   },
